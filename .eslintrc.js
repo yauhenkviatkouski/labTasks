46fe43caae3fe5cccb094fc14c4c081f853a7efc
Yauhen Kviatkouski <Yauhen.Kviatkouski@itechart-group.com>
2019-10-18 14:22:46 +0300

    init: es6 forbid

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    },
-    "plugins": [
-        "es5"
-    ]
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+  },
+  "plugins": [
+    "es5"
+  ],
+  "extends": [
+    "eslint:recommended",
+    "plugin:es5/no-es2015"
+  ]
 };
