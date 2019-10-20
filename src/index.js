98e970955e4e1b8acc0b4ffec1de1a94a3a6e18f
yauhenkviatkouski <kvetkovski@gmail.com>
2019-10-21 00:05:40 +0300

    feat: add calculator

--- src/index.js
+++ src/index.js
@@ -1,32 +1,57 @@
+function stringCalculator(expr) {
 
-function _parser(str, inputScheme) {
-  var components = {};
-  var defaultScheme = (inputScheme) ? inputScheme : 'DDMMYYYY';
-  components.day = str.substr([defaultScheme.indexOf('DD')], 2);
-  components.month = str.substr([defaultScheme.indexOf('MM')], 2);
-  components.year = str.substr([defaultScheme.indexOf('YYYY')], 4);
-  return components;
+  function check(str) {
+    var bracketsConfig = ['(', ')'];
+    var brackets = str.match(/[\(\)]/g);
+    for (var i = 0; i <= brackets.length; i++) {
+      if (brackets.length === 0) return true;
+      if ((brackets[i] === bracketsConfig[0] && brackets[i + 1] === bracketsConfig[1])) {
+        brackets.splice(i, 2)
+        i = (i === 0) ? i - 1 : i - 2;
+        continue;
+      }
+    }
+    return false;
+  }
+
+  if (expr.indexOf('(') !== -1 || expr.indexOf(')') !== -1) {
+    if (!check(expr)) throw ('ExpressionError: Brackets must be paired')
+  }
+
+  expr = expr.replace(/\s/g, '');
+
+  function operationSingleSolve(exprShort) {
+    var operandA = Number(exprShort.match(/\-?\d+\.*\d*/g)[0]);
+    exprShort = exprShort.replace(/\-?\d+\.*\d*/, '');
+    var operation = exprShort.match(/[\-\+\*\/]/)[0];
+    exprShort = exprShort.replace(/[\-\+\*\/]/, ' ');
+    var operandB = Number(exprShort.match(/\-?\d+\.*\d*/g)[0]);
+    switch (operation) {
+      case '+': return operandA + operandB;
+      case '-': return operandA - operandB;
+      case '*': return operandA * operandB;
+      case '/':
+        if (operandB === 0) throw ('TypeError: Devision by zero.');
+        return operandA / operandB;
+    }
+  }
+  function operationManySolve(exprLong) {
+    while ((exprLong.match(/\-?\d+\.*\d*/g).length > 1) && (exprLong.indexOf('/') !== -1 || exprLong.indexOf('*') !== -1)) {
+      exprLong = exprLong.replace(/((?<!\d)(\-?))\d+\.*\d*((e-1\d)?)[\*\/]\-?\d+\.*\d*/, operationSingleSolve);
+    }
+    while ((exprLong.match(/\-?\d+\.*\d*/g).length > 1) && (exprLong.indexOf('+') !== -1 || exprLong.indexOf('-') !== -1)) {
+      exprLong = exprLong.replace(/((?<!\d)(\-?))\d+\.*\d*((e-1\d)?)[\+\-]\-?\d+\.*\d*/, operationSingleSolve);
+    }
+    exprLong = exprLong.replace(/[()]/g, '');
+    return exprLong;
+  }
+
+  while (expr.indexOf('(') !== -1) {
+    expr = expr.replace(/\([^(]+?\)/, operationManySolve)
+  }
+
+  return Number(operationManySolve(expr));
+
 }
 
-function _print(components, outputScheme) {
-  var defaultScheme = (outputScheme) ? outputScheme : 'DD/MM/YYYY';
-  var result = defaultScheme.replace(/DD/, components.day);
-  result = result.replace(/MM/, components.month);
-  result = result.replace(/YYYY/, components.year);
-  return result
-}
-
-
-function formatMs(ms, outputScheme) {
-  var date = new Date(Number(ms));
-  var components = {};
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  components.year = date.getFullYear();
-  components.day = (day < 10) ? '0' + day : day;
-  components.month = (month < 10) ? '0' + month : month;
-  console.log(components)
-  return _print(components, outputScheme);
-}
-
-console.log(formatMs(651654651651))
+console.log(stringCalculator(' (  6 + 28 + 18 - (  (  61 + 17 * 64 * 98  ) * (  61 / 53 * 47 / 36 * 98  ) + 82 + (  69 - 55 / (  62 * 77 / 88 - 52 / 10  ) - 42 - (  48 / 84 * 77 + 40 - 13  )  )  )  ) - 4 / 99 '))
