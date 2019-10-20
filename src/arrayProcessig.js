3991544193b2226ae630dbf6f143b3a0fa5c7af7
yauhenkviatkouski <kvetkovski@gmail.com>
2019-10-20 12:34:54 +0300

    feat: add array processing

---
+++ src/arrayProcessig.js
@@ -0,0 +1,100 @@
+(function () {
+  var arrayProcessig = {};
+
+  arrayProcessig.subSum_n2 = function (array) {
+    try {
+      array = JSON.parse(array);
+      if (!Array.isArray(array)) throw new Error;
+    } catch (e) {
+      return "It isn't array of numbers!"
+    }
+
+    var maxSubArray = 0;
+    for (var i = 0; i < array.length; i += 1) {
+      for (var j = i; j < array.length; j += 1) {
+        var subArray = array.slice(i, j + 1);
+        console.log(subArray);
+        var subArrayValue = subArray.reduce(function (sum, current) {
+          return sum + current;
+        });
+        if (subArrayValue > maxSubArray) {
+          maxSubArray = subArrayValue;
+        }
+      }
+    }
+    return maxSubArray;
+  }
+
+  arrayProcessig.subSum_n = function (array) {
+    try {
+      array = JSON.parse(array);
+      if (!Array.isArray(array)) throw new Error;
+    } catch (e) {
+      return "It isn't array of numbers!"
+    }
+
+    var maxSubArray = 0;
+    var accumulator = 0;
+    for (var i = 0; i < array.length; i += 1) {
+      accumulator = accumulator + array[i];
+      if (accumulator < 0) {
+        accumulator = 0;
+      } else if (accumulator > maxSubArray) {
+        maxSubArray = accumulator
+      }
+    }
+    return maxSubArray;
+  }
+
+  arrayProcessig.searchMin = function (array) {
+    try {
+      array = JSON.parse(array);
+      if (!Array.isArray(array)) throw new Error;
+    } catch (e) {
+      return "It isn't array of numbers!"
+    }
+
+    return array.reduce(function (previous, item) {
+      return (item < previous) ? item : previous;
+    });
+  }
+
+  arrayProcessig.searchMax = function (array) {
+    try {
+      array = JSON.parse(array);
+      if (!Array.isArray(array)) throw new Error;
+    } catch (e) {
+      return "It isn't array of numbers!"
+    }
+
+    return array.reduce(function (previous, item) {
+      return (item > previous) ? item : previous;
+    });
+  }
+
+  arrayProcessig.searchMedian = function (array) {
+    try {
+      array = JSON.parse(array);
+      if (!Array.isArray(array)) throw new Error;
+    } catch (e) {
+      return "It isn't array of numbers!"
+    }
+
+    function quickSort(arr) {
+      if (arr.length === 0) return [];
+      var leftPart = [];
+      var rightPart = [];
+      var base = arr[0];
+      for (var i = 1; i < arr.length; i += 1) {
+        if (arr[i] < base) leftPart.push(arr[i]);
+        else rightPart.push(arr[i]);
+      }
+      return quickSort(leftPart).concat(base, quickSort(rightPart));
+    }
+    var sort = quickSort(array);
+    if (sort.length % 2 === 1) return sort[Math.floor(sort.length / 2)];
+    else return (sort[Math.floor(sort.length / 2 - 1)] + sort[Math.floor(sort.length / 2)]) / 2;
+  }
+
+  window.arrayProcessig = arrayProcessig;
+})();
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-var ar = '[2, -1, 2, 3, -9]';
+var ar = '[ 2, 3, -9, 11]';
 
 
-function subSum(array) {
-  try {
-    array = JSON.parse(array);
-    if (!Array.isArray(array)) throw new Error;
-  } catch (e) {
-    return "It isn't array of numbers!"
-  }
 
 
-  var maxSubArray = 0;
-  for (var i = 0; i < array.length; i += 1) {
-    for (var j = i; j < array.length; j += 1) {
-      var subArray = array.slice(i, j + 1);
-      var subArrayValue = subArray.reduce(function (sum, current) {
-        return sum + current;
-      });
-      if (subArrayValue > maxSubArray) {
-        maxSubArray = subArrayValue;
-      }
-    }
-  }
-  return maxSubArray;
-}
 
-// console.log(subSum(ar));
+console.log(searchMedian(ar));
 
