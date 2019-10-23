108089630b7ede7b15e8f894a5f70eb34d524abd
Yauhen Kviatkouski <Yauhen.Kviatkouski@itechart-group.com>
2019-10-23 18:00:57 +0300

    draft

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]; // 153
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Problem 1: Partial Application + Problem 2: Currying ///////////
 function bind() {
