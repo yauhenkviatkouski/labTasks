9f61f01e21188470a4acc73ff78f3c46224bac83
Yauhen Kviatkouski <Yauhen.Kviatkouski@itechart-group.com>
2019-10-31 14:20:50 +0300

    refactor: textformatter after review

--- p2-0/src/textFormatter.js
+++ p2-0/src/textFormatter.js
@@ -18,61 +18,81 @@ export default class TextFormatter {
     return str;
   }
 
+  cutLastSpace(str, resultStr, maxSize, startSubStr) {
+    const subStr = str.slice(startSubStr, startSubStr + maxSize);
+    const lastSpace = subStr.lastIndexOf(' ');
+    if (lastSpace < 1) {
+      return resultStr += subStr + '\n';
+    } else {
+      return resultStr += subStr.slice(0, lastSpace) + '\n';
+    }
+  }
+
+  cutOnSymbol(str, options) {
+    const maxSize = parseInt(options.maxSize);
+    let result = '';
+    let startSubStr = 0;
+    while (startSubStr < str.length) {
+      result += str.slice(startSubStr, startSubStr + maxSize) + '\n';
+      startSubStr += maxSize;
+    }
+    return result;
+  }
+
+  cutOnWord(str, options) {
+    const maxSize = parseInt(options.maxSize);
+    let result = '';
+    let startSubStr = 0;
+    while (startSubStr < str.length) {
+      result = this.cutLastSpace(str, result, maxSize, startSubStr);
+      startSubStr = result.lastIndexOf('\n') + 1;
+    }
+    return result;
+  }
+
+  cutOnSentence(str, options) {
+    const maxSize = parseInt(options.maxSize);
+    let result = '';
+    let startSubStr = 0;
+    while (startSubStr < str.length) {
+      const subStr = str.slice(startSubStr, startSubStr + maxSize);
+      const lastSentenceEnd = Math.max(
+          subStr.lastIndexOf('. '),
+          subStr.lastIndexOf('! '),
+          subStr.lastIndexOf('? ')
+      );
+      if (lastSentenceEnd < 1) {
+        result = this.cutLastSpace(str, result, maxSize, startSubStr);
+        startSubStr = result.lastIndexOf('\n') + 1;
+      } else {
+        result += subStr.slice(0, lastSentenceEnd + 1) + '\n';
+        startSubStr += 1 + lastSentenceEnd;
+      }
+    }
+    return result;
+  }
+
   format(str, options) {
     options.type = options.type || 'symbol';
     const maxSize = parseInt(options.maxSize);
-
     if (maxSize >= 0) {
-      let result = '';
-      let startSubStr = 0;
-      const cutLastSpace = (subStr) => {
-        const lastSpace = subStr.lastIndexOf(' ');
-        if (lastSpace < 1) {
-          result += subStr + '\n';
-          startSubStr += maxSize;
-        } else {
-          result += subStr.slice(0, lastSpace) + '\n';
-          startSubStr += 1 + lastSpace;
-        }
-      };
-
       switch (options.type) {
         case 'symbol': {
-          while (startSubStr < str.length) {
-            result += str.slice(startSubStr, startSubStr + maxSize) + '\n';
-            startSubStr += maxSize;
-          }
+          str = this.cutOnSymbol(str, options);
           break;
         }
         case 'word': {
-          while (startSubStr < str.length) {
-            const subStr = str.slice(startSubStr, startSubStr + maxSize);
-            cutLastSpace(subStr);
-          }
+          str = this.cutOnWord(str, options);
           break;
         }
         case 'sentence': {
-          while (startSubStr < str.length) {
-            const subStr = str.slice(startSubStr, startSubStr + maxSize);
-            const lastSentenceEnd = Math.max(
-                subStr.lastIndexOf('. '),
-                subStr.lastIndexOf('! '),
-                subStr.lastIndexOf('? ')
-            );
-            if (lastSentenceEnd < 1) {
-              cutLastSpace(subStr);
-            } else {
-              result += subStr.slice(0, lastSentenceEnd + 1) + '\n';
-              startSubStr += 1 + lastSentenceEnd;
-            }
-          }
+          str = this.cutOnSentence(str, options);
           break;
         }
         default: {
           break;
         }
       }
-      str = result;
     }
 
     return this.cutMaxStrings(str, options.maxStrings);
